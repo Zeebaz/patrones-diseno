c10b250e341a0679ee9ba14f58e8b008804a1a6f
Zeebaz <jairosebastianrp@gmail.com>
2025-02-14 16:15:38 -0600

    Prototype pattern implemented

--- 01-creacionales/04-prototype.ts
+++ 01-creacionales/04-prototype.ts
@@ -9,3 +9,48 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+class Document {
+  public title: string;
+  private content: string;
+  public author: string;
+
+  constructor(title: string, content: string, author: string) {
+    this.title = title;
+    this.content = content;
+    this.author = author;
+  }
+
+  clone(): Document {
+    return new Document(this.title, this.content, this.author);
+  }
+
+  displayInfo(): void {
+    console.log(`
+      Title: ${this.title}
+      Author: ${this.author}
+      Content: ${this.content}`);
+  }
+}
+
+function main() {
+  const document1 = new Document("Cotización", "500 dolares", "Author 1");
+  console.log({ InstanceaDoc1: document1 });
+  document1.displayInfo();
+
+  const document2 = document1.clone(); // usando patron
+  document2.title = "Cotización 2";
+  console.log({ InstanceaDoc2: document2 });
+  document2.displayInfo();
+
+  const document3 = { ...document1 };
+  document3.title = "Cotización 3";
+  console.log({ InstanceaDoc3: document3 });
+  // document3.displayInfo(); // pierde el prototype Document
+
+  const document4 = structuredClone(document1);
+  document4.title = "Cotización 4";
+  console.log({ InstanceaDoc4: document4 });
+  // document4.displayInfo(); // pierde el prototype Document
+}
+main();
--- 01-creacionales/04.2-prototype.ts
+++ 01-creacionales/04.2-prototype.ts
@@ -11,26 +11,30 @@
  */
 
 class Pokemon {
-  name: string;
-  type: string;
-  level: number;
-  attacks: string[];
+  // name: string;
+  // type: string;
+  // level: number;
+  // attacks: string[];
 
-  constructor(name: string, type: string, level: number, attacks: string[]) {
-    throw new Error('Method not implemented.');
-  }
+  constructor(
+    public name: string,
+    public type: string,
+    public level: number,
+    public attacks: string[]
+  ) {}
 
   // Método para clonar el Pokémon
   clone(): Pokemon {
     // Los ataques deben de evitar pasarse por referencia, es decir, no deben de ser el mismo arreglo.
     // Completar: Debe devolver un nuevo Pokémon con los mismos atributos
+    return new Pokemon(this.name, this.type, this.level, [...this.attacks]);
   }
 
   displayInfo(): void {
     console.log(
-      `Nombre: ${this.name}\nTipo: ${this.type}\nNivel: ${
+      `> ${this.name}\n - Tipo: ${this.type}\n - Nivel: ${
         this.level
-      }\nAtaques: ${this.attacks.join(', ')}`
+      }\n - Ataques: ${this.attacks.join(", ")}\n`
     );
   }
 }
@@ -41,11 +45,14 @@ class Pokemon {
 // 3. Llamar a displayInfo en cada Pokémon para mostrar sus detalles.
 
 // Ejemplo:
-// const basePokemon = new Pokemon("Charmander", "Fuego", 1, ["Llamarada", "Arañazo"]);
-// const clone1 = basePokemon.clone();
-// clone1.name = "Charmeleon";
-// clone1.level = 16;
-// clone1.attacks.push("Lanzallamas");
+const basePokemon = new Pokemon("Charmander", "Fuego", 1, [
+  "Llamarada",
+  "Arañazo",
+]);
+const clone1 = basePokemon.clone();
+clone1.name = "Charmeleon";
+clone1.level = 16;
+clone1.attacks.push("Lanzallamas");
 
-// basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
-// clone1.displayInfo();
+basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
+clone1.displayInfo();
